2a44c33f13515e8261f631737409402ee8bb6171
Bruce Wayne <Ameenub@Ameen.AMEEN>
2017-09-25 18:24:53 +0100

    update

--- src/main.js
+++ src/main.js
@@ -2,36 +2,32 @@
 module.exports = {
 	aritGeo:(arr) => {
 		let numLen = arr.length;
-		let initValue = arr[0];
 		let diffArit = arr[0] - arr[1];
-		let diffGeo = arr[0] / arr[1];
-		for(let i=0; i < arr.length-1; i++){
-			if(arr[i+1] - arr[i-1] !== diffArit){
+        let diffGeo = arr[0] / arr[1];
+        let arithmetic = true;
+        let geometric = true;
+		for(let i=0; i < numLen-1; i++){
+			if(arr[i+1] - arr[i] !== diffArit){
 				let arithmetic= false;
 			}
-			else{
-				let arithmetic = true;
-			}
-			if (arr[i+1] - arr[i-1] !==diffGeo){
+			if (arr[i+1] / arr[i] !== diffGeo){
 				let geometric = false;
-			}else{
-				let geometric = true;
 			}
         }
-        if(arithmetic = true){
+        if(arithmetic === true){
             return "arithmetic";
         }
         else if(geometric === true){
             return "geometric";
         }
-		else if(arr !== arithmetic && geometric){
+		else if((arr !== arithmetic )&& (arr !==geometric)){
 			return -1;
 		}
 		else if (numLen ===" "){
 			return 0;
 		}
 		else{
-			return "Aleins are real";
+			return "";
 		}
 	}
 }
--- tests/tests.js
+++ tests/tests.js
@@ -2,33 +2,21 @@ const assert = require('chai').assert;
 const myApp = require('../src/main.js');
 
 describe("aritGeo", function(){
-    
     describe("Handles valid input", function(){
         it("Should return arithmetic", function(){
         assert.equal(myApp.aritGeo([5, 7, 9, 11, 13, 15]),"arithmetic")
         });
-    })
-
-    describe("Handles empty input", function(){
-        it("should return zero if no input", function(){
-        assert.equal(myApp.aritGeo(" "),0)
-        });
-    })
-
-    describe("Handles valid input", function(){
         it("Should return Geometric", function(){
         assert.equal(myApp.aritGeo([2, 4, 8, 16]),"Geometirc")
         });
-    })
+        it("Should return geomtric", function(){
+            assert.equal(myApp.aritGeo([16, 8, 4, 2]),"Geometric")
+            });
+        it("Should return Arithmetic", function(){
+            assert.equal(myApp.aritGeo()[15, 13, 11, 9, 7])
+            });
 
-    describe("Handles input for non aritmetic and geometric sequence", function(){
-        it("Should return negative 1 (-1)", function(){
-        assert.equal(myApp.aritGeo([10, 28, 12, 9]),-1)
-        });
-    })
-
-    describe("Handles valid input", function(){
-        it("should retur geometric", function(){
+        it("should return geometric", function(){
         assert.equal(myApp.aritGeo([3, 9, 27]),"Geometric")
         });
     })
@@ -37,29 +25,20 @@ describe("aritGeo", function(){
         it("Should return 'invalid input'", function(){
         assert.equal(myApp.aritGeo("arithmetic"),"invalid input")
         });
-    })
 
-    describe("handles single input", function(){
         it("Should fail", function(){
         assert.equal(myApp.aritGeo(1),"invalid")
         });
-    })
 
-    describe("Handles valid input", function(){
-        it("Should return geomtric", function(){
-        assert.equal(myApp.aritGeo([16, 8, 4, 2]),"Geometric")
+        it("should return zero if no input", function(){
+            assert.equal(myApp.aritGeo(" "),0)
         });
-    })
 
-    describe("Handles valid input", function(){
-        it("Should return Arithmetic", function(){
-        assert.equal(myApp.aritGeo()[15, 13, 11, 9, 7])
-        });
-    })
-
-    describe("Handles invalid input", function(){
         it("should return invalid", function(){
         assert.equal(myApp.aritGeo([" "]),"value must be entered")
         });
+        it("Should return negative 1 (-1)", function(){
+            assert.equal(myApp.aritGeo([10, 28, 12, 9]),-1)
+            });
     })
 })
